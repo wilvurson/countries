a8abc454699b24b1f613fbf4778a674beda3cb4e
wilvurson <wilvurson@gmail.com>
2025-08-29 15:19:56 +0800

    Update pagination.js

--- useEffect/main-holder/pagination.js
+++ useEffect/main-holder/pagination.js
@@ -1,21 +1,25 @@
 "use client";
 
-export default function Pagination() {
-    return (
-      <div className="flex justify-center mt-8 space-x-2">
-        <button className="px-3 py-1 rounded bg-gray-200 hover:bg-gray-300">
-          1
+export default function Pagination({ currentPage, onPageChange }) {
+  return (
+    <div className="flex justify-center mt-8 space-x-2">
+      {[1, 2, 3].map((page) => (
+        <button
+          key={page}
+          onClick={() => onPageChange(page)}
+          className={`px-3 py-1 rounded ${
+            currentPage === page ? "bg-gray-400 text-white" : "bg-gray-200 hover:bg-gray-300"
+          }`}
+        >
+          {page}
         </button>
-        <button className="px-3 py-1 rounded bg-gray-200 hover:bg-gray-300">
-          2
-        </button>
-        <button className="px-3 py-1 rounded bg-gray-200 hover:bg-gray-300">
-          3
-        </button>
-        <span className="px-3 py-1 text-gray-400">...</span>
-        <button className="px-3 py-1 rounded bg-gray-200 hover:bg-gray-300">
-          Next
-        </button>
-      </div>
-    );
+      ))}
+      <button
+        onClick={() => onPageChange(currentPage + 1)}
+        className="px-3 py-1 rounded bg-gray-200 hover:bg-gray-300"
+      >
+        Next
+      </button>
+    </div>
+  );
 }
