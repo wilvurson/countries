8764c7c34594c46a107b7ef50e47b4847f708de6
wilvurson <wilvurson@gmail.com>
2025-08-29 15:35:45 +0800

    Update page.jsx

--- useEffect/page.jsx
+++ useEffect/page.jsx
@@ -7,23 +7,29 @@ import Pagination from "./main-holder/pagination";
 
 export default function App() {
   const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const limit = 12; // must match Products.jsx
 
   return (
     <div className="min-h-screen bg-gray-50">
       <Header />
 
-      {/* Products */}
       <main className="p-6 max-w-6xl mx-auto">
         <h2 className="text-xl font-semibold text-gray-700 mb-4 mt-15">
           Products (Page {page})
         </h2>
 
         <div className="flex flex-wrap gap-x-9 gap-y-5">
-          <Products page={page} />
+          <Products page={page} onTotalChange={setTotal} />
         </div>
 
         {/* Pagination */}
-        <Pagination currentPage={page} onPageChange={setPage} />
+        <Pagination
+          currentPage={page}
+          total={total}
+          limit={limit}
+          onPageChange={setPage}
+        />
       </main>
     </div>
   );
